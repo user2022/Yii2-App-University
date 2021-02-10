c4fd1438875dd8244a71760678e34e5493d0ab54
User2022 <marleydabb55@gmail.com>
2021-02-10 01:59:32 +0000

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql.scm.tees.ac.uk;dbname=t7230883',
-    'username' => 't7230883',
-    'password' => 'UNTt=Kc4$@.6',
-    'charset' => 'utf8',
-];
